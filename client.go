5afd5d22909cdf547cb76c887bdfa595da770684
Josef Karasek <jkarasek@redhat.com>
2017-03-02 11:32:22 +0100

    query server in loop

--- client.go
+++ client.go
@@ -1,3 +1,24 @@
+/*
+Copyright (c) 2017 Josef Karasek
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 package main
 
 import (
@@ -8,34 +29,40 @@ import (
   "net/http"
   "encoding/json"
   "io/ioutil"
+  "sync"
+  "time"
+  "strconv"
 )
 
 type item struct {
   CatalogId int  `json:"catalogId"`
-  Amount int     `json:"amount"`
+  Amount    int     `json:"amount"`
 }
 
 type order struct {
-  Name string     `json:"name"`
+  Name    string     `json:"name"`
   Address string  `json:"address"`
-  Items []item    `json:"items"`
+  Items   []item    `json:"items"`
 }
 
 type action struct {
-  action int
+  action     int
+  id         int
   badRequest bool
 }
 
 type connection struct {
-  Url string `json:"url"`
+  Url  string `json:"url"`
   Port int   `json:"port"`
 }
 
 const (
-  QUERY_ITEMS = iota
+  QUERY_ITEMS             = iota
   QUERY_ITEM_AVAILABILITY
   ORDER_ITEM
 )
+
+var wg sync.WaitGroup
 
 func parseOperation(op string) int {
   var opResult int
@@ -58,19 +85,35 @@ func parseOperation(op string) int {
 func checkArgs() (*action, error) {
   switch len(os.Args) {
   case 2:
-    return &action{parseOperation(os.Args[1]), false}, nil
+    return &action{parseOperation(os.Args[1]), -1, false}, nil
   case 3:
     if os.Args[2] == "--error" {
-      return &action{parseOperation(os.Args[1]), true}, nil
+      return &action{parseOperation(os.Args[1]), -1, true}, nil
+    }
+    if val, err := strconv.Atoi(os.Args[2]); err == nil {
+      return &action{parseOperation(os.Args[1]), val, false}, nil
+    }
+  case 4:
+    if val, err := strconv.Atoi(os.Args[2]); err == nil && os.Args[3] == "--error" {
+      return &action{parseOperation(os.Args[1]), val, true}, nil
     }
   }
   return nil, errors.New(getHelp())
 }
 
-func queryItems(config *connection) error {
+func queryItems(config *connection, isId int) error {
+  if isId == -1 {
+    return queryAllCatalogs(config)
+  } else {
+    return queryCatalog(config, isId)
+  }
+}
+
+func queryCatalog(config *connection, id int) error {
   client := &http.Client{}
-  for categoty, _ := range []int{0,1,2} {
-    url := fmt.Sprintf("http://%s:%d/catalog/list/%d", config.Url, config.Port, categoty)
+  for true {
+    start := time.Now()
+    url := fmt.Sprintf("http://%s:%d/catalog/list/%d", config.Url, config.Port, id)
     request, err := http.NewRequest("GET", url, nil)
     if err != nil {
       return err
@@ -84,66 +127,62 @@ func queryItems(config *connection) error {
     if err != nil {
       return err
     }
-    prettyPrintJsonBody(body)
+    elapsed := time.Since(start)
+    fmt.Printf("Time elapsed: %s | Response: %s\n", elapsed.String(), string(body))
     response.Body.Close()
+
+    time.Sleep(2000 * time.Millisecond)
   }
   return nil
 }
 
-func queryAvailability(config *connection) error {
-  url := fmt.Sprintf("http://%s:%d/availability/5", config.Url, config.Port)
+func queryAllCatalogs(config *connection) error {
+  client := &http.Client{}
+  for true {
+    for category, _ := range []int{0, 1, 2} {
+      start := time.Now()
+      url := fmt.Sprintf("http://%s:%d/catalog/list/%d", config.Url, config.Port, category)
+      request, err := http.NewRequest("GET", url, nil)
+      if err != nil {
+        return err
+      }
+      request.Header.Set("Accept", "application/json")
+      response, err := client.Do(request)
+      if err != nil {
+        return err
+      }
+      body, err := ioutil.ReadAll(response.Body)
+      if err != nil {
+        return err
+      }
+      elapsed := time.Since(start)
+      fmt.Printf("Time elapsed: %s | Response: %s\n", elapsed.String(), string(body))
+      response.Body.Close()
+    }
+    time.Sleep(2000 * time.Millisecond)
+  }
+  return nil
+}
+
+func getId(isId, _default int) int {
+  if isId == -1 {
+    return _default
+  } else {
+    return isId
+  }
+}
+
+func queryAvailability(config *connection, isId int) error {
+  url := fmt.Sprintf("http://%s:%d/availability/%d", config.Url, config.Port, getId(isId, 5))
   client := &http.Client{}
   request, err := http.NewRequest("GET", url, nil)
   if err != nil {
     return err
   }
   request.Header.Set("Accept", "application/json")
-  response, err := client.Do(request)
-  if err != nil {
-    return err
-  }
-  body, err := ioutil.ReadAll(response.Body)
-  if err != nil {
-    return err
-  }
-  prettyPrintJsonBody(body)
-  response.Body.Close()
-  return nil
-}
 
-func createBadOrder() []order {
-  return []order{{
-    Name: "error",
-    Address: "badhood",
-    Items: []item{{5,1}},
-  }}
-}
-
-func orderItem(config *connection, badRequest bool) error {
-  var orders []order
-  var err error
-  if badRequest {
-    orders = createBadOrder()
-  } else {
-    orders, err = readOrders("./orders")
-    if err != nil {
-      return err
-    }
-  }
-  url := fmt.Sprintf("http://%s:%d/order", config.Url, config.Port)
-  client := &http.Client{}
-  for _, order := range orders {
-    orderJson, err := json.Marshal(order)
-    if err != nil {
-      return err
-    }
-
-    request, err := http.NewRequest("PUT", url, bytes.NewBuffer(orderJson))
-    if err != nil {
-      return err
-    }
-    request.Header.Set("Content-Type", "application/json")
-    request.Header.Set("Accept", "application/json")
+  for true {
+    start := time.Now()
     response, err := client.Do(request)
     if err != nil {
       return err
@@ -152,38 +191,113 @@ func orderItem(config *connection, badRequest bool) error {
     if err != nil {
       return err
     }
-    prettyPrintJsonBody(body)
+    elapsed := time.Since(start)
+    fmt.Printf("Time elapsed: %s | Response: %s\n", elapsed.String(), string(body))
     response.Body.Close()
+
+    time.Sleep(2000 * time.Millisecond)
   }
   return nil
 }
 
-func prettyPrintJsonBody(body []byte) {
-  var out bytes.Buffer
-  json.Indent(&out, body, "", "  ")
-  fmt.Println(out.String())
+func createBadOrder() *order {
+  return &order{
+    Name:    "error",
+    Address: "badhood",
+    Items:   []item{{5, 1}},
+  }
 }
 
-func readOrders(dirName string) ([]order, error) {
+func orderItem(config *connection, badRequest bool) error {
+  var newOrder *order
+  var err error
+  if badRequest {
+    newOrder = createBadOrder()
+  } else {
+    newOrder, err = readOrders("./orders")
+    if err != nil {
+      return err
+    }
+  }
+  url := fmt.Sprintf("http://%s:%d/order", config.Url, config.Port)
+  client := &http.Client{}
+
+  // Server side has limit of 20 connections
+  // If we wanna block the server we need to block all of those threads
+  var connections int
+  if badRequest {
+    connections = 20
+  } else {
+    connections = 1
+  }
+
+  orderJson, err := json.Marshal(newOrder)
+  if err != nil {
+    return err
+  }
+
+  for true {
+    wg.Add(connections)
+    for i := 0; i < connections; i++ {
+      go func(id int) {
+        start := time.Now()
+        request, err := http.NewRequest("PUT", url, bytes.NewBuffer(orderJson))
+        if err != nil {
+          fmt.Printf("%d: Error %s\n", id, err)
+          return
+        }
+        request.Header.Set("Content-Type", "application/json")
+        request.Header.Set("Accept", "application/json")
+        response, err := client.Do(request)
+        if err != nil {
+          fmt.Printf("%d: Error %s\n", id, err)
+          return
+        }
+        body, err := ioutil.ReadAll(response.Body)
+        if err != nil {
+          fmt.Printf("%d: Error %s\n", id, err)
+          return
+        }
+        elapsed := time.Since(start)
+        fmt.Printf("Time elapsed: %s | Response: %s\n", elapsed.String(), string(body))
+        response.Body.Close()
+        wg.Done()
+      }(i)
+    }
+    wg.Wait()
+    time.Sleep(2000 * time.Millisecond)
+  }
+  return nil
+}
+
+func prettyPrintBody(body []byte) {
+  var out bytes.Buffer
+  // check if body is json
+  err := json.Indent(&out, body, "", "  ")
+  if err != nil {
+    fmt.Println(string(body))
+  } else {
+    fmt.Println(out.String())
+  }
+}
+
+func readOrders(dirName string) (*order, error) {
   files, err := ioutil.ReadDir(dirName)
   if err != nil {
     return nil, err
   }
-  var orders = make([]order, len(files))
-  for i, file := range files {
-    dat, err := ioutil.ReadFile("./orders/" + file.Name())
-    if err != nil {
-      fmt.Println("Error while opening: " + file.Name() + "\n" + err.Error())
-      continue
-    }
-    var buff order
-    if err := json.Unmarshal(dat, &buff); err != nil {
-      fmt.Println("Not a JSON file: " + file.Name())
-      continue
-    }
-    orders[i] = buff
+  if len(files) == 0 {
+    return nil, errors.New("No files found in ./orders")
   }
-  return orders, nil
+  dat, err := ioutil.ReadFile("./orders/" + files[0].Name())
+  if err != nil {
+    return nil, err
+  }
+  var buff order
+  if err := json.Unmarshal(dat, &buff); err != nil {
+    return nil, err
+  }
+  return &buff, nil
 }
 
 func configure() (*connection, error) {
@@ -209,20 +323,18 @@ func main() {
     fmt.Println(err)
     os.Exit(1)
   }
-
   switch action.action {
   case QUERY_ITEMS:
     fmt.Println("Operation: QUERY_ITEMS")
-    queryItems(config)
+    err = queryItems(config, action.id)
   case QUERY_ITEM_AVAILABILITY:
     fmt.Println("Operation: QUERY_ITEM_AVAILABILITY")
-    queryAvailability(config)
+    err = queryAvailability(config, action.id)
   case ORDER_ITEM:
     fmt.Println("Operation: ORDER_ITEM")
     err = orderItem(config, action.badRequest)
   default:
-    fmt.Println(getHelp())
-    os.Exit(1)
+    err = errors.New(getHelp())
   }
   if err != nil {
     fmt.Println(err)
@@ -231,9 +343,29 @@ func main() {
 }
 
 func getHelp() string {
-  return fmt.Sprint("Usage: client OPERATION [--error]\n" +
-                    "OPERATION\n" +
-                    "  * items\n" +
-                    "  * availability\n" +
-                    "  * order")
+  return fmt.Sprint("Usage: client OPERATION [item_id] [--error]\n" +
+    "\n" +
+    "OPERATION\n" +
+    "  * items\n" +
+    "  * availability\n" +
+    "  * order\n" +
+    "\n" +
+    "Examples:\n" +
+    "  # List items in all categories\n" +
+    "  client items\n" +
+    "\n" +
+    "  # List items in category 0 (available categories are 0,1,2)\n" +
+    "  client items 0\n" +
+    "\n" +
+    "  # Query availability of item with id 1\n" +
+    "  client availability 1\n" +
+    "\n" +
+    "  # Query availability of an arbitrary item\n" +
+    "  client availability\n" +
+    "\n" +
+    "  # Send order from 'orders' dir to server\n" +
+    "  client order\n" +
+    "\n" +
+    "  # Send order to a slow endpoint\n" +
+    "  client order --error")
 }
